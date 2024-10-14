e078ceac5997b2bfbf6e318d77f7cc34ba9dc641
Aden Neal <adenocbenji@gmail.com>
2024-10-14 13:01:16 -0500

    fixed file structure

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,65 @@
+export default function Projects() {
+  return (
+    <div>
+       <section className="portfolio" id="portfolio">
+        <h2 className="heading">Latest <span>Project</span></h2>
+        <div className="portfolio-container">
+            <div className="portfolio-box">
+                <img src="./fitnesslabpic.jpg" alt="" />
+                <div className="portfolio-layer">
+                    <h4>Fitness Lab</h4>
+                    <a href="https://zainabid333.github.io/Fitness-lab/"><i className='bx bx-link-external'>Site</i></a>
+                    <a href="https://github.com/zainabid333/Fitness-lab"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+
+            <div className="portfolio-box">
+                <img src="./1_Mt9jzw_LqwSEnTiSKokc3A.png" alt="" />
+                <div className="portfolio-layer">
+                    <h4>Node/sequelize</h4>
+                    <a href="https://github.com/adenman/Challenge-13-ORM"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+
+            <div className="portfolio-box">
+                <img src="./weather.webp" alt="" />
+                <div className="portfolio-layer">
+                    <h4>Weather Dashboard</h4>
+                    
+                    <a href="https://adenman.github.io/Challenge---6"><i className='bx bx-link-external'>Site</i></a>
+                    <a href="https://github.com/adenman/Challenge---6"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+
+            <div className="portfolio-box">
+                <img src="./note.png" alt="" />
+                <div className="portfolio-layer">
+                    <h4>Note Taker</h4>
+                    
+                    <a href="https://note-taker-1-erm7.onrender.com/"><i className='bx bx-link-external'>Site</i></a>
+                    <a href="https://github.com/adenman/Note-taker"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+
+            <div className="portfolio-box">
+                <img src="./PWA.png" alt="" />
+                <div className="portfolio-layer">
+                    <h4>PWA</h4>
+                    <a href="https://challenge-19-pwa-1.onrender.com"><i className='bx bx-link-external'>Site</i></a>
+                    <a href="https://github.com/adenman/Challenge-19-PWA"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+
+            <div className="portfolio-box">
+                <img src="./portfolio.webp" alt="" />
+                <div className="portfolio-layer">
+                    <h4>CSS Portfolio</h4>
+                    <a href="https://adenman.github.io/Portfolio/"><i className='bx bx-link-external'>Site</i></a>
+                    <a href="https://github.com/adenman/Portfolio"><i className='bx bx-link-external'>Github</i></a>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  );
+}
